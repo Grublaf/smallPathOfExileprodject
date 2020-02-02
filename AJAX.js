40ebeb86abe5d265f76725f1c12163a1779cb9ae
Filip Alburg <filip.alburg@gmail.com>
2020-02-02 13:48:23 +0100

    Added comments and removed obselite code

--- AJAX.js
+++ AJAX.js
@@ -2,15 +2,15 @@ $.ajax({
     url: 'http://localhost:8080/characterList/kontusbonde',
     type: 'GET',
     headers: {
-        'Access-Control-Allow-Origin': '*'
-    }, // <-------- set this
-    dataType: 'json', // // <-------- use JSONP
+        'Access-Control-Allow-Origin': '*' //Allowing access controll from node server
+    },
+    dataType: 'json',
     success: function (data) {
-        $.each(data, function (i, obj) {
+        $.each(data, function (i, obj) { //Iterate the JSON
             if (obj.league == "Metamorph") {
                 $('.charactes').append('<p>' + obj.name + ' is playing in ' + obj.league + ' and is level ' + obj.level + '</p>');
             }
-            //use obj.id and obj.name here, for example:
+            //use obj.id and obj.name here, for example. Will return character name:
             console.log(obj.name);
 
         });
--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const {
 const rp = promisify(request);
 
 const app = express();
-app.use(cors());
-var logga = ((text) => {
+app.use(cors()); //Fixes some issues with CORS
+var logga = ((text) => { //Writes to test file for easier testing.
     fs.writeFile("./test", text, function (err) {
         if (err) {
             return console.log(err);
@@ -24,15 +24,15 @@ var logga = ((text) => {
     });
 });
 
-
+// Send request to URL/characterList/:user to get json response
 app.get('/characterList/:user', (req, res, next) => {
     rp('https://pathofexile.com/character-window/get-characters?accountName=' + req.params.user).then(({
         body,
         statusCode
     }) => {
-        var charList = JSON.parse(body);
+        var charList = JSON.parse(body); //Parse rp url with json(body)
         console.log("Type of:" + typeof charList);
-        charList.forEach(Char => {
+        charList.forEach(Char => { //Should remove the foreach later
             console.log("Char 1 function - " + JSON.stringify(Char.name) + JSON.stringify(Char.league));;
 
         });
@@ -40,38 +40,6 @@ app.get('/characterList/:user', (req, res, next) => {
         res.json(charList);
 
     });
-});
-/*
-rp('https://pathofexile.com/character-window/get-characters?accountName=Grublaf').then(({
-    body,
-    statusCode
-}) => {
-    var charList = JSON.parse(body);
-    console.log("Type of:" + typeof charList);
-    charList.forEach(Char => {
-        console.log("Char 1 function - " + JSON.stringify(Char.name) + JSON.stringify(Char.league));
-
-    });
-    for (var char in charList) {
-        console.log("Char second function - " + JSON.stringify(char));
-
-        // if (char.name.contains('Wankman')) {
-        //     // logga(JSON.stringify(char, null, 2));
-        // }
-        // res.write(char);
-        // res.send(char);
-    };
-    // res.send(charList);
-    // res.send(body);
-}); */
-
-app.get('/', (req, res) => {
-    console.log("Meepworked on /");
-    res.send("meep");
-});
-
-app.get('/public/:resource', (req, res) => {
-
 });
 
 app.listen(8080);
